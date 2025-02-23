47d4f2ba8419d05a77ff776da6b858b1141a585e
Barış SEMERCİ <barissemerci3@gmail.com>
2025-02-23 20:22:02 +0300

    delete redundant files

--- composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Platform.android.kt
+++
@@ -1,9 +0,0 @@
-package com.barissemerci.bookpedia
-
-import android.os.Build
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.barissemerci.bookpedia
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
