061e78cb6c688c71f8179a1712e1df2733079147
Barış SEMERCİ <barissemerci3@gmail.com>
2025-03-02 13:59:15 +0300

    add book list viewmodel parameters

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/App.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/App.kt
@@ -2,16 +2,32 @@ package com.barissemerci.bookpedia
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import com.plcoding.bookpedia.book.presentation.book_list.BookListScreenRoot
-import com.plcoding.bookpedia.book.presentation.book_list.BookListViewModel
+import com.barissemerci.bookpedia.book.data.network.KtorRemoteBookDataSource
+import com.barissemerci.bookpedia.book.data.repository.DefaultBookRepository
+import com.barissemerci.bookpedia.book.presentation.book_list.BookListScreenRoot
+import com.barissemerci.bookpedia.book.presentation.book_list.BookListViewModel
+import com.plcoding.bookpedia.core.data.HttpClientFactory
+import io.ktor.client.engine.HttpClientEngine
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
-fun App() {
+fun App(
+    engine: HttpClientEngine
+) {
 
     BookListScreenRoot(
-        viewModel = remember { BookListViewModel() },
+        viewModel = remember {
+            BookListViewModel(
+                bookRepository = DefaultBookRepository(
+                    remoteBookDataSource = KtorRemoteBookDataSource(
+                        httpClient = HttpClientFactory
+                            .create(engine = engine) // Different engine for different platforms
+                        // Android uses OkHttp, iOS uses Darwin, Desktop uses OkHttp
+                    )
+                )
+            )
+        },
         onBookClick = { book -> }
     )
 }
