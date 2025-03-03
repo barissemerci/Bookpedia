b9f07e03dba26674a0c982041b6ccc09ed462851
Barış SEMERCİ <barissemerci3@gmail.com>
2025-03-03 14:27:42 +0300

    remove /works/ prefix from SearchedBookDto id

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/mappers/BookMappers.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/mappers/BookMappers.kt
@@ -5,7 +5,7 @@ import com.barissemerci.bookpedia.book.domain.Book
 
 fun SearchedBookDto.toBook(): Book {
     return Book(
-        id = id,
+        id = id.substringAfterLast("/"), // remove the /works/ prefix
         title = title,
         imageUrl = if (coverKey != null) {
             "https://covers.openlibrary.org/b/olid/${coverKey}-L.jpg"
