5a21c4541f5eb926e0dd8a8666b6456e18532b0f
Barış SEMERCİ <barissemerci3@gmail.com>
2025-03-05 15:22:01 +0300

    update desktop app title

--- composeApp/src/desktopMain/kotlin/com/barissemerci/bookpedia/main.kt
+++ composeApp/src/desktopMain/kotlin/com/barissemerci/bookpedia/main.kt
@@ -9,7 +9,7 @@ fun main() {
     application {
         Window(
             onCloseRequest = ::exitApplication,
-            title = "CMP-Bookpedia",
+            title = "Bookpedia",
         ) {
             App()
         }
